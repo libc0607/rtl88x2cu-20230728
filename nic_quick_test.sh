3ef5c1a4e4d0ab094f973745e59208c9a45f3beb
libc0607 <libc0607@gmail.com>
2024-10-31 17:21:39 +0800

    Update test script, remove rtw88 driver first

--- nic_quick_test.sh
+++ nic_quick_test.sh
@@ -5,6 +5,10 @@ CHANNEL=$2
 
 echo "insmod"
 rmmod 88x2cu
+rmmod rtw88_8822cu
+rmmod rtw88_8822c
+rmmod rtw88_usb
+rmmod rtw88_core
 modprobe cfg80211
 insmod 88x2cu.ko rtw_regd_src=1 rtw_tx_pwr_by_rate=0 rtw_tx_pwr_lmt_enable=0
 
